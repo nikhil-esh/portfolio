2fe29f28df6e125a3e43e530419fda7f658ab0a0
nikhil-esh <nikhilesh.kr9@gmail.com>
2021-01-18 00:46:42 +0530

    videos-added-thumbnails

--- assets/js/video-player.js
+++
@@ -1,7 +0,0 @@
-var videoBuild = new YoutubeOverlayModule({
-    sourceUrl: 'https://www.youtube.com/embed/1Q8fG0TtVAY',
-    triggerElement: "#demo",
-    progressCallback: function() {
-        console.log("Callback Invoked.");
-    }
-});
